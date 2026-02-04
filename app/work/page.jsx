8bd1de09caada168026d62ad1768c65e0b3241f8
Mayank Saini <mayanksaini0416@gmail.com>
2026-02-04 14:52:41 +0530

    work pages first para removed

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -4,18 +4,10 @@ import WorkSection from "../components/WorkSection";
 const WorkPage = () => {
   return (
     <>
-      <PageIntro
-        eyebrow="Our work"
-        title="Proven solutions for real-world problems."
-      >
-        <p>
-          We believe in efficiency and maximizing our resources to provide the
-          best value to our clients. The primary way we do that is by re-using
-          the same five projects we’ve been developing for the past decade.
-        </p>
-      </PageIntro>
-      
+    <div className="mt-10">
+
       <WorkSection/>
+          </div>
     </>
   );
 };
